ba8ba00463cda264f99e352e1384ef59fbbe1702
paltimonte <paltimonte@gmail.com>
2020-05-08 17:55:18 -0400

    Chap 8 Silver Challenge - make data property private.

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,29 +1,28 @@
 (function (window) {
   'use strict';
   var App = window.App || {};
+  var data = {};
 
-  class DataStore {
-    constructor() {
-      this.data = {};
-    }
+  function DataStore() {
+
   }
 
   DataStore.prototype.add = function (key, val) {
-    this.data[key] = val;
+    data[key] = val;
   }
 
   DataStore.prototype.get = function (key) {
-    return this.data[key];
+    return data[key];
   }
 
   DataStore.prototype.getAll = function () {
-    return this.data;
+    return data;
   }
 
   DataStore.prototype.remove = function (key) {
     delete this.data[key];
   }
 
-  App.DataStore = DataStore;
+  App.DataStore = DataStore
   window.App = App;
 })(window);
